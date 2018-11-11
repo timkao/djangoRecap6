01d019d374ef2d6d7f328c8a2740357ce1e9b088
Tim Kao <pakuya.kao@gmail.com>
2018-11-11 17:23:46 -0500

    render article views

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from .models import Article
+
+class ArticleListView(ListView):
+  model = Article
+  template_name = 'article_list.html'
