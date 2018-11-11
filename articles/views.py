0fabfb74a3d163e532b6a73de1f06fd8c35d1802
Tim Kao <pakuya.kao@gmail.com>
2018-11-11 18:24:44 -0500

    creation view is done with better home page

--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Article
@@ -25,3 +25,8 @@ class ArticleDeleteView(DeleteView):
   model = Article
   template_name = 'article_delete.html'
   success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(CreateView):
+  model = Article
+  template_name = 'article_create.html'
+  fields = ('title', 'body', 'author')
