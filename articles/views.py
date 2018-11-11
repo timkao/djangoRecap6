5faf234178db221806c82c8070deacddb90d6fea
Tim Kao <pakuya.kao@gmail.com>
2018-11-11 18:57:51 -0500

    add loginrequiredmixin

--- articles/views.py
+++ articles/views.py
@@ -1,32 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Article
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
   model = Article
   template_name = 'article_list.html'
+  login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
   model = Article
   template_name = 'article_edit.html'
   fields = ('title', 'body')
+  login_url = 'login'
 
-
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
   model = Article
   template_name = 'article_detail.html'
+  login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
   model = Article
   template_name = 'article_delete.html'
   success_url = reverse_lazy('article_list')
+  login_url = 'login'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
   model = Article
   template_name = 'article_create.html'
-  fields = ('title', 'body', 'author')
+  fields = ('title', 'body')
+  login_url = 'login'
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
