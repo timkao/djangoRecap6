ee49d4b9161c31292154bb1c17b7e3ce57865a52
Tim Kao <pakuya.kao@gmail.com>
2018-11-12 19:21:45 -0500

    change article admin view

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Article
-from .models import Comment
+from .models import Article, Comment
 
-admin.site.register(Article)
+#class CommentInline(admin.StackedInline):
+  #model = Comment
+  #extra = 0
+
+class CommentInline(admin.TabularInline):
+  model = Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+  inlines = [
+    CommentInline,
+  ]
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
